4ccd3552c5de7956c6f58931b4e09d1cfe0260bd
电波系 <159118179+colerith@users.noreply.github.com>
2025-12-27 16:53:14 +0800

    Change base path for deployment to '/electric-wave/'

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 修改为根路径，适配 VPS 部署
+  base: '/electric-wave/', // 已适配为新的仓库名
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY)
   },
