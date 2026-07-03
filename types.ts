6e42364c2143d8fb7e752ab78c40e3626ea8502a
电波系 <159118179+colerith@users.noreply.github.com>
2026-07-03 18:08:03 +0800

    Auto-sync: Update data

--- types.ts
+++ types.ts
@@ -32,7 +32,7 @@ export interface SiteConfig {
 
 export const DATA_VERSION = {
   hash: 'he246ae52',
-  generatedAt: '2026-07-03T09:45:21.765Z'
+  generatedAt: '2026-07-03T10:08:02.246Z'
 } as const;
 
 export type ViewMode = 'gallery' | 'list';
